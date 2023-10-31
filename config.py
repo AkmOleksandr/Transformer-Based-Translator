c471c451633a2a340f508d9c5bd3d74eadc13dc2
AkmOleksandr <115898001+AkmOleksandr@users.noreply.github.com>
2023-10-31 11:32:05 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,38 @@
+# https://www.youtube.com/watch?v=ISNdQcPhsts&t=13s
+'''
+Config for robust access to hyparameters, paths and specifics of the dataset
+'''
+
+from pathlib import Path
+
+def get_config():
+    return {
+        "batch_size": 16,
+        "num_epochs": 3,
+        "lr": 10**-4,
+        "seq_len": 350,
+        "d_model": 512,
+        "datasource": 'opus_books',
+        "lang_src": "en", # input language
+        "lang_trgt": "it", # output language
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": "latest",
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}{epoch}.pt"
+    return str(Path('.') / model_folder / model_filename)
+
+# Find the latest weights file in the weights folder
+def latest_weights_file_path(config):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
