ba0ff554ac5969b87765aa92db541eea39a59a62
Oleksandr <akm.oleksandr@gmail.com>
2023-11-03 11:48:59 -0500

    Alex commited

--- config.py
+++ config.py
@@ -23,15 +23,13 @@ def get_config():
     }
 
 def get_weights_file_path(config, epoch: str):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+    return str(Path('.') / config['model_folder'] / model_filename)
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
-    model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}*"
-    weights_files = list(Path(model_folder).glob(model_filename))
+    weights_files = list(Path(config['model_folder']).glob(model_filename))
     if len(weights_files) == 0:
         return None
     weights_files.sort()
