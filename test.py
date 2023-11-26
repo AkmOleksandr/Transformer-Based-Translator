1907970721a701b3f79cf46109ac1554234b05ad
Oleksandr <akm.oleksandr@gmail.com>
2023-11-25 21:23:11 -0600

    updt

--- test.py
+++
@@ -1,6 +0,0 @@
-from datasets import load_dataset
-from datasets import IterableDataset
-from config import get_config
-
-dataset = load_dataset('csv', data_files='eng-ukr-dataset/data.csv')
-print(dataset)
