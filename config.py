70ee326c6b20e6a2c53b100bd8629b38b5d938c4
AkmOleksandr <115898001+AkmOleksandr@users.noreply.github.com>
2023-12-01 12:17:30 -0600

    Update config.py

--- config.py
+++ config.py
@@ -21,14 +21,15 @@ def get_config():
     }
 
 def get_weights_file_path(config, epoch: str):
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / config['model_folder'] / model_filename)
+    return str(Path('.') / model_folder / model_filename)
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
-    base_model_path = Path(config['base_model_path'])
-    model_path = base_model_path.joinpath(latest_weights_file_path(config))
-    weights_files = list(Path(config['model_folder']).glob(model_path))
+    model_folder = f"{config['datasource']}_{config['model_folder']}"
+    model_filename = f"{config['model_basename']}*"
+    weights_files = list(Path(model_folder).glob(model_filename))
     if len(weights_files) == 0:
         return None
     weights_files.sort()
