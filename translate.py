8178cf62924ae0e90a0fb26abf879d856260d426
AkmOleksandr <115898001+AkmOleksandr@users.noreply.github.com>
2023-12-01 12:01:15 -0600

    Update translate.py

--- translate.py
+++ translate.py
@@ -7,34 +7,28 @@ from dataset import BilingualDataset
 import torch
 import sys
 
-def get_translation(sentence: str):
+def translate(sentence: str):
     # Define the device, tokenizers, and model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
     config = get_config()
-    base_tokenizer_path = Path(config['base_tokenizer_path'])
-    tokenizer_src_path = base_tokenizer_path.joinpath(config['tokenizer_file'].format(config['lang_src']))
-    tokenizer_trgt_path = base_tokenizer_path.joinpath(config['tokenizer_file'].format(config['lang_trgt']))
-    print(f"tokenizer_src_path: {tokenizer_src_path}")
-    print(f"tokenizer_trgt_path: {tokenizer_trgt_path}")
-    tokenizer_src = Tokenizer.from_file(str(tokenizer_src_path))
-    tokenizer_trgt = Tokenizer.from_file(str(tokenizer_trgt_path))
-    model = build_transformer(tokenizer_src.get_vocab_size(), tokenizer_trgt.get_vocab_size(), config["seq_len"], config['seq_len'], d_model=config['d_model']).to(device)
+    tokenizer_src = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_src']))))
+    tokenizer_tgt = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_tgt']))))
+    model = build_transformer(tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size(), config["seq_len"], config['seq_len'], d_model=config['d_model']).to(device)
 
     # Load the pretrained weights
-    model_path = latest_weights_file_path(config)
-    print(f"model_path: {model_path}")
-    state = torch.load(model_path)
+    model_filename = latest_weights_file_path(config)
+    state = torch.load(model_filename)
     model.load_state_dict(state['model_state_dict'])
 
     # if the sentence is a number use it as an index to the test set
     label = ""
     if type(sentence) == int or sentence.isdigit():
         id = int(sentence)
-        ds = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_trgt']}", split='all')
-        ds = BilingualDataset(ds, tokenizer_src, tokenizer_trgt, config['lang_src'], config['lang_trgt'], config['seq_len'])
+        ds = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='all')
+        ds = BilingualDataset(ds, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
         sentence = ds[id]['src_text']
-        label = ds[id]["trgt_text"]
+        label = ds[id]["tgt_text"]
     seq_len = config['seq_len']
 
     # translate the sentence
@@ -52,7 +46,7 @@ def get_translation(sentence: str):
         encoder_output = model.encode(source, source_mask)
 
         # Initialize the decoder input with the sos token
-        decoder_input = torch.empty(1, 1).fill_(tokenizer_trgt.token_to_id('[SOS]')).type_as(source).to(device)
+        decoder_input = torch.empty(1, 1).fill_(tokenizer_tgt.token_to_id('[SOS]')).type_as(source).to(device)
 
         # Print the source sentence and target start prompt
         if label != "": print(f"{f'ID: ':>12}{id}") 
@@ -62,24 +56,30 @@ def get_translation(sentence: str):
 
         # Generate the translation word by word
         while decoder_input.size(1) < seq_len:
-            # build mask for target and calculate output
+            # Build mask for target and calculate output
             decoder_mask = torch.triu(torch.ones((1, decoder_input.size(1), decoder_input.size(1))), diagonal=1).type(torch.int).type_as(source_mask).to(device)
             out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
 
-            # project next token
+            # Project next token
             prob = model.project(out[:, -1])
             _, next_word = torch.max(prob, dim=1)
             decoder_input = torch.cat([decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
 
-            # print the translated word
-            print(f"{tokenizer_trgt.decode([next_word.item()])}", end=' ')
+            # Print the translated word
+            print(f"{tokenizer_tgt.decode([next_word.item()])}", end=' ')
 
-            # break if we predict the end of sentence token
-            if next_word == tokenizer_trgt.token_to_id('[EOS]'):
+            # Break if we predict the end of sentence token
+            if next_word == tokenizer_tgt.token_to_id('[EOS]'):
                 break
 
-    # convert ids to tokens
+    # Convert ids to tokens
+    return tokenizer_tgt.decode(decoder_input[0].tolist())
+    
+# Read sentence from argument
+translate(sys.argv[1] if len(sys.argv) > 1 else "I am not a very good a student.")
+
+    # Convert ids to tokens
     return tokenizer_trgt.decode(decoder_input[0].tolist())
     
-#read sentence from argument
+# Read sentence from argument
 get_translation(sys.argv[1] if len(sys.argv) > 1 else "I am not a very good a student.")
