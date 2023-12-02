34a09a70e7907818f972d3e0ed07a8f768cb9a67
AkmOleksandr <115898001+AkmOleksandr@users.noreply.github.com>
2023-12-01 18:56:14 -0600

    Update translate.py

--- translate.py
+++ translate.py
@@ -12,8 +12,8 @@ def get_translation(config, sentence: str):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
     tokenizer_src = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_src']))))
-    tokenizer_tgt = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_tgt']))))
-    model = build_transformer(tokenizer_src.get_vocab_size(), tokenizer_tgt.get_vocab_size(), config["seq_len"], config['seq_len'], d_model=config['d_model']).to(device)
+    tokenizer_trgt = Tokenizer.from_file(str(Path(config['tokenizer_file'].format(config['lang_trgt']))))
+    model = build_transformer(tokenizer_src.get_vocab_size(), tokenizer_trgt.get_vocab_size(), config["seq_len"], config['seq_len'], d_model=config['d_model']).to(device)
 
     # Load the pretrained weights
     model_filename = latest_weights_file_path(config)
@@ -24,10 +24,10 @@ def get_translation(config, sentence: str):
     label = ""
     if type(sentence) == int or sentence.isdigit():
         id = int(sentence)
-        ds = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='all')
-        ds = BilingualDataset(ds, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+        ds = load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_trgt']}", split='all')
+        ds = BilingualDataset(ds, tokenizer_src, tokenizer_trgt, config['lang_src'], config['lang_trgt'], config['seq_len'])
         sentence = ds[id]['src_text']
-        label = ds[id]["tgt_text"]
+        label = ds[id]["trgt_text"]
     seq_len = config['seq_len']
 
     # Initialize an empty string to store the translation
@@ -47,7 +47,7 @@ def get_translation(config, sentence: str):
         encoder_output = model.encode(source, source_mask)
 
         # Initialize the decoder input with the sos token
-        decoder_input = torch.empty(1, 1).fill_(tokenizer_tgt.token_to_id('[SOS]')).type_as(source).to(device)
+        decoder_input = torch.empty(1, 1).fill_(tokenizer_trgt.token_to_id('[SOS]')).type_as(source).to(device)
 
         # Print the source sentence and target start prompt
         if label != "": print(f"{f'ID: ':>12}{id}") 
@@ -67,13 +67,13 @@ def get_translation(config, sentence: str):
             decoder_input = torch.cat([decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1)
 
             # Build the translated word
-            translated_word = tokenizer_tgt.decode([next_word.item()])
+            translated_word = tokenizer_trgt.decode([next_word.item()])
 
             # Append the translated word to the result string
             translation_result += translated_word + ' '
 
             # Break if we predict the end of sentence token
-            if next_word == tokenizer_tgt.token_to_id('[EOS]'):
+            if next_word == tokenizer_trgt.token_to_id('[EOS]'):
                 break
 
     # Convert ids to tokens
